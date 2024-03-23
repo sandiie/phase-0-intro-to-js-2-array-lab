c6d50960c860756765163f066bf4f45423bbb971
+ <+>
2024-03-23 15:22:22 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+let cats= ["Milo" , "Otis" , "Garfield"]  
+
+function destructivelyAppendCat(Pebbles){
+    cats.push(Pebbles);
+  }
+  function destructivelyRemoveLastCat(Pebbles){
+    cats.pop(Pebbles);
+  }
+
+function destructiveRemoveFirstCat(Otis){
+    cats.shift(Otis);
+}
+
+function appendCat(Broom){
+    let newCats =["Milo" , "Otis" , "Garfield" , "Broom"]
+    return newCats;
+}
+function destructivelyPrependCat (Sarabi){
+    cats.unshift(Sarabi)
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo)
+}
+function prependCat(Arnold){
+    let newCats = ["Arnold" ,"Milo" ,"Otis" ,"Garfield"  ] 
+    return newCats;
+}
+function removeLastCat (){
+let newCats = cats.slice(0, -1);
+    return newCats;
+}
+function removeFirstCat () {
+    let newCats = cats.slice(1);
+    return newCats;
+}
